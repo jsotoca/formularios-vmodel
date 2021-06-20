9c44fc6d64bca5cc21d1e8dcf47436cc75863b0c
jsotoca <jsotoca@unprg.edu.pe>
2021-06-20 11:36:48 -0500

    Configuracion inicial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <form>
+    <input type="text" class="form-control my-2" placeholder="ingrese nombre" v-model="tarea.texto" />
+  </form>
+  <p>
+    {{tarea}}
+  </p>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      tarea : {
+        texto: "",
+        descripcion: "",
+        categorias: []
+      }
+    };
+  },
+};
 </script>
